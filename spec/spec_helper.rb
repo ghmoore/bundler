d42c64959a956292d988d1a5987ecc525795202b
David Rodríguez <deivid.rodriguez@riseup.net>
2019-03-12 21:08:26 +0100

    Remove unused filter

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -96,7 +96,6 @@ RSpec.configure do |config|
 
   config.filter_run_excluding :ruby => LessThanProc.with(RUBY_VERSION)
   config.filter_run_excluding :rubygems => LessThanProc.with(Gem::VERSION)
-  config.filter_run_excluding :git => LessThanProc.with(git_version)
   config.filter_run_excluding :rubygems_master => (ENV["RGV"] != "master")
   config.filter_run_excluding :bundler => LessThanProc.with(Bundler::VERSION.split(".")[0, 2].join("."))
   config.filter_run_excluding :ruby_repo => !(ENV["BUNDLE_RUBY"] && ENV["BUNDLE_GEM"]).nil?
